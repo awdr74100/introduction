3689a6b0dad83125f60167ec73b01b8f65c4680f
a78945612385238@gmail.com <a78945612385238@gmail.com>
2019-04-03 23:29:42 +0800

    44

--- js/test.js
+++ js/test.js
@@ -6,11 +6,12 @@ a.addEventListener("mouseup",bt2,false);
 
 function bt1 (){
     b.textContent = "還沒開始更新拉 幹!";
-    a.textContent = "返回";
-    b.className = "title1";
+    
+    b.className = "bbb";
+    
 }
 function bt2 (){
-    b.textContent = "藍奕濡的個人介紹";
+    b.textContent = "藍奕濡的個人介紹網站";
     a.textContent = "看更多";
     b.className = "title";
 }
